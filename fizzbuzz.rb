75526979727a5b86ce0332a704cab3942bcd11f3
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-13 17:15:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,7 +8,9 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
-  else int % 3 == 0 && int % 5 == 0 
+  elsif int % 3 == 0 && int % 5 == 0  
     "FizzBuzz"
+  else 
+    "nill"
   end
 end
