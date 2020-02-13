a94889ea32e27ba34d6ab9c315ed81a385835ff3
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-13 16:55:09 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,4 @@
 require_relative '/home/looping-post-0686/rspec-fizzbuzz-online-web-sp-000/fizzbuzz.rb'
-require_relative '../fizzbuzz.rb'
 
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
