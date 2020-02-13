4fbbed064bbd9d79c1feb641dc64e2be3d76c13c
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-13 16:58:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,6 @@
+require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
